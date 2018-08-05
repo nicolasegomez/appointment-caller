d897fd2b8534a907068b39e83489b9fd9f96277b
Nico Gómez <nicolas.e.gomez@gmail.com>
2018-08-05 15:41:03 -0300

    [ESH-5336] Init project

---
+++ src/main/java/com/escribehost/appointmentcaller/AppointmentcallerApplication.java
@@ -0,0 +1,12 @@
+package com.escribehost.appointmentcaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentcallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentcallerApplication.class, args);
+	}
+}
